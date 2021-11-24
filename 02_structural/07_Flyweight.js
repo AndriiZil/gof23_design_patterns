8acfbeb32ff9d9db567018b2d4bd38a31a681bad
Andrii <andrii.zilnyk@gmail.com>
2021-11-24 14:58:05 +0200

    added new patterns

--- 02_structural/07_Flyweight.js
+++ 02_structural/07_Flyweight.js
@@ -1,1 +1,46 @@
+// Cache / Легковес
 
+class Auto {
+    constructor(model) {
+        this.model = model;
+    }
+}
+
+class AutoFactory {
+    constructor() {
+        this.models = {};
+    }
+
+    create(name) {
+        let model = this.models[name];
+        if (model) {
+            return model;
+        }
+        this.models[name] = new Auto(name);
+
+        return this.models[name];
+    }
+
+    getModels() {
+        console.table(this.models);
+    }
+}
+
+const factory = new AutoFactory();
+
+const bmw = factory.create('BMW');
+const audi = factory.create('Audi');
+const tesla = factory.create('Tesla');
+const blackTesla = factory.create('Tesla'); // Returned from Cache
+
+factory.getModels();
+/**
+ *   ┌─────────┬─────────┐
+     │ (index) │  model  │
+     ├─────────┼─────────┤
+     │   BMW   │  'BMW'  │
+     │  Audi   │ 'Audi'  │
+     │  Tesla  │ 'Tesla' │
+     └─────────┴─────────┘
+ *
+ */
